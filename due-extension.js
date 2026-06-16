f73a6e542039b5f7aa063ca35b0baa47cd11cf81
LMonte19 <lissymontero19@gmail.com>
2026-06-16 01:47:11 -0400

    Bump partners detail cache version

--- due-extension.js
+++ due-extension.js
@@ -2,5 +2,5 @@ import "./admin-tools.js?v=1";
 import "./batch-tools.js?v=1";
 import "./menu-page-fix.js?v=1";
 import "./loan-view-fix.js?v=2";
-import "./partners-page-fix.js?v=3";
+import "./partners-page-fix.js?v=4";
 import "./settings-validation-fix.js?v=1";
