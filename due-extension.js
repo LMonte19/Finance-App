2e3488e8bcbf0c720b48ed85a9be18f26a19bb25
LMonte19 <lissymontero19@gmail.com>
2026-06-16 01:39:46 -0400

    Load settings validation helpers

--- due-extension.js
+++ due-extension.js
@@ -2,4 +2,5 @@ import "./admin-tools.js?v=1";
 import "./batch-tools.js?v=1";
 import "./menu-page-fix.js?v=1";
 import "./loan-view-fix.js?v=2";
-import "./partners-page-fix.js?v=2";
+import "./partners-page-fix.js?v=3";
+import "./settings-validation-fix.js?v=1";
