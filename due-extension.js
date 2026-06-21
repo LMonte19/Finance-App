8cb28cbffe33fb2079b1fcd33e6a9c4536a3c861
LMonte19 <lissymontero19@gmail.com>
2026-06-21 11:40:11 -0400

    Load activity history page

--- due-extension.js
+++ due-extension.js
@@ -6,3 +6,4 @@ import "./partners-page-fix.js?v=4";
 import "./settings-validation-fix.js?v=1";
 import "./loan-actions.js?v=1";
 import "./payment-management.js?v=3";
+import "./activity-log.js?v=1";
