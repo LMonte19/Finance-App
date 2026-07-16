0a1bc41c01faa45f176526e7934c3d92e3bfa23d
LMonte19 <lissymontero19@gmail.com>
2026-07-16 00:27:45 -0400

    Load smooth calendar refinements

--- due-extension.js
+++ due-extension.js
@@ -16,6 +16,6 @@ import './account-ui.js?v=1';
 import './client-detail-desktop.js?v=1';
 import './client-detail-polish.js?v=1';
 import './client-profile-tabs.js?v=2';
-import './client-profile-refinements.js?v=1';
+import './client-profile-refinements.js?v=2';
 
 console.log('safe module loader active');
