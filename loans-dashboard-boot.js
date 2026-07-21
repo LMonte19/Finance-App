23d4b1ed9bc062b11ebbf6d074bb1a8b5585484d
LMonte19 <lissymontero19@gmail.com>
2026-07-20 22:36:51 -0400

    Load loans dashboard polish layer

--- loans-dashboard-boot.js
+++ loans-dashboard-boot.js
@@ -8,7 +8,9 @@ function addStyle(id,href){
 
 addStyle('loansDashboardCss','./loans-dashboard.css?v=2');
 addStyle('loansDashboardOverridesCss','./loans-dashboard-overrides.css?v=2');
+addStyle('loansDashboardPolishCss','./loans-dashboard-polish.css?v=1');
 
 import('./loans-dashboard.js?v=2')
   .then(()=>import('./loans-dashboard-transition-fix.js?v=1'))
+  .then(()=>import('./loans-dashboard-polish.js?v=1'))
   .catch(error=>console.error('loans dashboard failed to load',error));
