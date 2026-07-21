209bbd4a685fad171dc8d63c7a7afa0d07a389f0
LMonte19 <lissymontero19@gmail.com>
2026-07-20 23:34:08 -0400

    Refresh Loans dashboard corrections module

--- loans-dashboard-boot.js
+++ loans-dashboard-boot.js
@@ -16,5 +16,5 @@ import('./loans-dashboard.js?v=2')
   .then(()=>import('./loans-dashboard-transition-fix.js?v=1'))
   .then(()=>import('./loans-dashboard-polish.js?v=2'))
   .then(()=>import('./loans-dashboard-period.js?v=1'))
-  .then(()=>import('./loans-dashboard-corrections.js?v=1'))
+  .then(()=>import('./loans-dashboard-corrections.js?v=2'))
   .catch(error=>console.error('loans dashboard failed to load',error));
